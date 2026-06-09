6190fd85a833eab53178c56068a31b7bc7cc1d8a
earthtraveller1 <liangyuezhao7@gmail.com>
2026-06-08 20:45:24 -0400

    Moved the GLFW over to the vendor directory. This is so that the GitHub language counter doesn't count the GLFW things.

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 # Of course, we first need to build the deps, but only if the dependencies 
 # are not already built
-if (!(Test-Path -Path "glfw/src/glfw3.lib"))
+if (!(Test-Path -Path "vendor/glfw/src/glfw3.lib"))
 {
-    cmake -S glfw -B glfw `
+    cmake -S vendor/glfw -B vendor/glfw `
         -G Ninja -D CMAKE_C_COMPILER=clang `
         -D GLFW_BUILD_TESTS=OFF -D GLFW_BUILD_EXAMPLES=OFF -D GLFW_BUILD_DOCS=OFF
     cmake --build glfw
@@ -11,8 +11,8 @@ if (!(Test-Path -Path "glfw/src/glfw3.lib"))
 $CC = "clang"
 $CFLAGS = "-Wall", "-Wpedantic", "-Wextra" 
 
-$INCLUDES = "-I$env:VULKAN_SDK/Include", "-Iglfw/include"
-$LIBS = "$env:VULKAN_SDK/Lib/vulkan-1.lib", "glfw/src/glfw3.lib", "-luser32", "-lshell32", "-lgdi32" 
+$INCLUDES = "-I$env:VULKAN_SDK/Include", "-Ivendor/glfw/include"
+$LIBS = "$env:VULKAN_SDK/Lib/vulkan-1.lib", "vendor/glfw/src/glfw3.lib", "-luser32", "-lshell32", "-lgdi32" 
 
 & $CC $CFLAGS $INCLUDES -o main main.c $LIBS
 
