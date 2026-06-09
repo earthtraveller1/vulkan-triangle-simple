9a6b55766700216ff0c7fc0f14eda45513c5e2dc
earthtraveller1 <liangyuezhao7@gmail.com>
2026-06-08 20:35:05 -0400

    Added a compile commands database so that clangd works.

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 # Of course, we first need to build the deps, but only if the dependencies 
 # are not already built
-if (!(Test-Path -Path "glfw/src/glfw3.lib")) {
+if (!(Test-Path -Path "glfw/src/glfw3.lib"))
+{
     cmake -S glfw -B glfw `
         -G Ninja -D CMAKE_C_COMPILER=clang `
         -D GLFW_BUILD_TESTS=OFF -D GLFW_BUILD_EXAMPLES=OFF -D GLFW_BUILD_DOCS=OFF
@@ -14,3 +15,18 @@ $INCLUDES = "-I$env:VULKAN_SDK/Include", "-Iglfw/include"
 $LIBS = "$env:VULKAN_SDK/Lib/vulkan-1.lib", "glfw/src/glfw3.lib", "-luser32", "-lshell32", "-lgdi32" 
 
 & $CC $CFLAGS $INCLUDES -o main main.c $LIBS
+
+# If the compilation database doesn't exist, we generate it, of course
+if (!(Test-Path -Path "compile_commands.json"))
+{
+    $command = @(
+        [PSCustomObject]@{
+            directory = "$PWD"
+            arguments = @($CC) + $CFLAGS + $INCLUDES + @("-o", "main", "main.c") + $LIBS
+            file = "main.c"
+        }
+    )
+
+    $command_json = $command | ConvertTo-Json
+    Set-Content -Path "compile_commands.json" -Value $command_json
+}
