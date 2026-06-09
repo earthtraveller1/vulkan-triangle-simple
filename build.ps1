4e6c0b094b315a6e99161761b031c69e97aa24fc
earthtraveller1 <liangyuezhao7@gmail.com>
2026-06-08 20:11:42 -0400

    Changed the windows thing to use the bundled GLFW instead of the system glfw.

--- build.ps1
+++ build.ps1
@@ -1,7 +1,13 @@
 $CC = "clang"
 $CFLAGS = "-Wall", "-Wpedantic", "-Wextra" 
 
-$INCLUDES = "-I$VULKAN_SDK/Include", "-I./vendor/glfw/include"
-$LIBS = "-L$VULKAN_SDK/Lib", "-L./vendor/glfw/libs", "-lvulkan-1", "-lglfw3"
+$INCLUDES = "-I$VULKAN_SDK/Include", "-Iglfw/include"
+$LIBS = "$VULKAN_SDK/Lib/vulkan-1.lib", "glfw/src/glfw3.lib"
+
+# Obviously we first need to build GLFW
+cmake -S glfw -B glfw `
+    -G Ninja -D CMAKE_C_COMPILER=$CC `
+    -D GLFW_BUILD_TESTS=OFF -D GLFW_BUILD_EXAMPLES=OFF -D GLFW_BUILD_DOCS=OFF
+cmake --build glfw
 
 & $CC $CFLAGS $INCLUDES -o main main.c $LIBS
